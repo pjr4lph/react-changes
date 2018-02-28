b59f3943f8bc58542685bf200f8ffe0cc680538b
ralph salazar <ralph.slzr@gmail.com>
2018-02-27 19:00:19 -0500

    server changes

--- server/server.js
+++ server/server.js
@@ -2,30 +2,44 @@
 // how does the user look up past lists? by date?
 // what happens when an item is removed from the list?
 // when something is removed save to database by date or title?
-
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const http = require('http');
-
 const app = express();
+
 // making a server makes it possible to use same server for websockets
 // const server = http.createServer(app);
 
 // the database is going to save removed todoList items by date and list item
+const dbURI = 'mongodb://localhost:27017/';
 
+mongoose.connect(dbURI);
+const db = mongoose.connection;
 
-app.get('/', function (req, res) {
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+  console.log('Connected to Database');
+});
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './../index.html'));
 });
 
-app.get('/styles.css', function(req, res) {
+app.get('/styles.css',(req, res) => {
   res.sendFile(path.join(__dirname, './../styles.css'));
 });
 
-app.get('/build/webpack-bundle.js', function (req, res) {
+app.get('/build/webpack-bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname , './../build/webpack-bundle.js'));
 });
+
+// app.get('/find', (req, res, next) => {
+//
+// });
 
 app.listen('3000');
