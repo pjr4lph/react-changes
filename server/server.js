da3d45e0226846271258cce60968918d8cb36de8
ralph salazar <ralph.slzr@gmail.com>
2018-07-04 15:08:09 -0400

    update package and cluster set up

--- server/server.js
+++ server/server.js
@@ -1,43 +1,63 @@
-// the server is collecting the current pagelist and saving to database according to date
-// how does the user look up past lists? by date?
-// what happens when an item is removed from the list?
-// when something is removed save to database by date or title?
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const http = require('http');
-const app = express();
-const todoController = require('./../todoController')
-// making a server makes it possible to use same server for websockets
-// const server = http.createServer(app);
+const cluster = require('cluster');
 
-// the database is going to save removed todoList items by date and list item
-const dbURI = 'mongodb://ralph:pjr4lph@ds151558.mlab.com:51558/ralphs';
+if (cluster.isMaster) {
+  let numWorkers = require('os').cpus().length;
 
-mongoose.connect(dbURI);
-const db = mongoose.connection;
+  console.log(`Master cluster setting up ${numWorkers} workers`);
 
-db.on('error', console.error.bind(console, 'connection error:'));
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
 
-db.once('open', () => {
-  console.log('Connected to Database');
-});
+  cluster.on('online', function(worker) {
+    console.log(`Worker ${worker.process.pid} is online`);
+  });
 
-app.use(bodyParser.json());
+  cluster.on('exit', function(worker, code, signal) {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+    console.log(`Starting a new worker`);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './../index.html'));
-});
+    cluster.fork();
+  });
+} else {
 
-app.get('/styles.css',(req, res) => {
-  res.sendFile(path.join(__dirname, './../styles.css'));
-});
+  const express = require('express');
+  const path = require('path');
+  const mongoose = require('mongoose');
+  const bodyParser = require('body-parser');
+  const http = require('http');
+  const app = express();
+  const todoController = require('./../todoController')
+  // making a server makes it possible to use same server for websockets
+  // const server = http.createServer(app);
 
-app.get('/build/webpack-bundle.js', (req, res) => {
-  res.sendFile(path.join(__dirname , './../build/webpack-bundle.js'));
-});
+  // the database is going to save removed todoList items by date and list item
+  const dbURI = 'mongodb://ralph:pjr4lph@ds151558.mlab.com:51558/ralphs';
 
-app.post('/addTodo', (req, res) => {console.log(todoController.createTodo)});
+  mongoose.connect(dbURI);
+  const db = mongoose.connection;
 
-app.listen('3000', () => console.log('listening on 3000'));
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', () => {
+    console.log('Connected to Database');
+  });
+
+  app.use(bodyParser.json());
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './../index.html'));
+  });
+
+  app.get('/styles.css',(req, res) => {
+    res.sendFile(path.join(__dirname, './../styles.css'));
+  });
+
+  app.get('/build/webpack-bundle.js', (req, res) => {
+    res.sendFile(path.join(__dirname , './../build/webpack-bundle.js'));
+  });
+
+  app.post('/addTodo', (req, res) => {console.log(todoController.createTodo)});
+
+  app.listen('3000', () => console.log('listening on 3000'));
+}
