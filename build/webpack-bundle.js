7414f149035e623045d03d119630570bb84d1837
ralph salazar <ralph.slzr@gmail.com>
2018-07-04 14:41:08 -0400

    save changes

--- build/webpack-bundle.js
+++ build/webpack-bundle.js
@@ -18347,39 +18347,84 @@ module.exports = App;
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(1);
 
 var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var List = function List(_ref) {
-  var pageList = _ref.pageList,
-      removeItem = _ref.removeItem;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-  var renderedList = pageList.map(function (obj, i) {
-    var curDate = Date(obj.createdAt).toString();
-    return _react2.default.createElement(
-      'li',
-      { key: i, onClick: function onClick() {
-          removeItem(obj.value);
-        } },
-      obj.value,
-      _react2.default.createElement(
-        'span',
-        null,
-        curDate
-      )
-    );
-  });
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-  return _react2.default.createElement(
-    'ul',
-    null,
-    renderedList
-  );
-};
+// const List = ({pageList, removeItem}) => {
+var List = function (_Component) {
+  _inherits(List, _Component);
+
+  function List(props) {
+    _classCallCheck(this, List);
+
+    var _this = _possibleConstructorReturn(this, (List.__proto__ || Object.getPrototypeOf(List)).call(this, props));
+
+    _this.handleSubmit = _this.handleSubmit.bind(_this);
+    return _this;
+  }
+
+  _createClass(List, [{
+    key: 'handleSubmit',
+    value: function handleSubmit(e) {
+      e.preventDefault();
+      // const data = new FormData(e.currentTarget);
+      console.log(e.target);
+      var data = e.target.value;
+
+      fetch('localhost:3000/addTodo', {
+        method: 'POST',
+        body: data
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      var renderedList = this.props.pageList.map(function (obj, i) {
+        var curDate = Date(obj.createdAt).toString();
+        return _react2.default.createElement(
+          'li',
+          { key: i, onClick: function onClick() {
+              _this2.props.removeItem(obj.value);
+            } },
+          obj.value,
+          _react2.default.createElement(
+            'span',
+            null,
+            curDate
+          )
+        );
+      }, this);
+      // <div>
+      // </div>
+      return _react2.default.createElement(
+        'form',
+        { onSubmit: function onSubmit(e) {
+            _this2.handleSubmit(e);
+          } },
+        _react2.default.createElement(
+          'ul',
+          null,
+          renderedList
+        )
+      );
+    }
+  }]);
+
+  return List;
+}(_react.Component);
 
 module.exports = List;
 
